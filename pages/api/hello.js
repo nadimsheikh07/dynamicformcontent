2dd911fe69c3d417c564ebce0c7f0474dddfc587
nadim <nadim.sheikh.07@gmail.com>
2021-03-22 22:37:14 +0530

    update content

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import React from "react";
+import pdfHelper from "../../helper/pdfHelper";
+import LeaveContent from "../../content/leave";
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+export default async (req, res) => {
+  const { name, schoolName } = req.query
+  const buffer = await pdfHelper.componentToPDFBuffer(
+    <LeaveContent name={name} schoolName={schoolName} />
+  );
+
+  // prompt to download pdf
+  // res.setHeader('Content-disposition', 'attachment; filename="article.pdf');
+
+  // set content type
+  res.setHeader('Content-Type', 'application/pdf');
+
+  // output the pdf buffer
+  res.end(buffer);
 }
